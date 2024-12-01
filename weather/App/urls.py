53ae77ce7dbc6bca79f625ff3a8854b51a76e72b
rahulkale12 <work.rk12@gmail.com>
2024-12-01 23:41:31 +0530

    added blogs template

--- weather/App/urls.py
+++ weather/App/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index , name="index"),
+    path('blog_create/', views.blog_create, name="blog_create"),
+    path('blogs/', views.blogs, name="blogs"),
 ]
--- weather/App/views.py
+++ weather/App/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def blog_create(request):
+    return render(request, "blog_create.html")
+
+def blogs(request):
+    return render(request, "blogs.html")
